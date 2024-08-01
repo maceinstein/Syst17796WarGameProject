fce060a01eb9c731578b02da40d54535eed28f5c
harneet2003 <kau16367@sheridancollege.ca>
2024-08-01 00:39:55 -0400

    mid term

---
+++ src/ca/sheridancollege/project/WarCard.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author User
+ */
+public class WarCard {
+    
+    private String rank;
+    private String suit;
+
+    public WarCard(String rank, String suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+    
+    public int getRankValue() {
+        switch (rank) {
+            case "2": return 2;
+            case "3": return 3;
+            case "4": return 4;
+            case "5": return 5;
+            case "6": return 6;
+            case "7": return 7;
+            case "8": return 8;
+            case "9": return 9;
+            case "10": return 10;
+            case "J": return 11;
+            case "Q": return 12;
+            case "K": return 13;
+            case "A": return 14;
+            default: return 0;
+        }
+    }
+    
+    
+    
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
+    
+}
